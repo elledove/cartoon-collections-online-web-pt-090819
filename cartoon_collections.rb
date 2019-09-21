cf43e4590c3ad33138fc9aa1c071f03f402e0028
Lauren Dove <elledoveinc@gmail.com>
2019-09-21 17:06:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(theseven)
+theseven.each_with_index do |name , index | 
+  puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+                
+def summon_captain_planet(planeteers)
+  planeteers.map do |name|
+    name.capitalize << "!"
+  end
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(finding)
+  if finding.length == 4
+    return true 
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(type)           # code an argument  
+                           # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  type.include? ()
 end
